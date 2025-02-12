795c229f5d429c63bc64ba523049df7287815995
Serhii Romanov <sromanov@sharpminds.com>
2025-02-12 18:53:31 +0200

    Introduce ContentBuilder class to split logic for different content types

--- src/app/endpoints/content.py
+++ src/app/endpoints/content.py
@@ -1,3 +1,5 @@
+from typing import Dict, List
+
 from fastapi import APIRouter, Request
 
 from app.dependencies import JellyFinDep
@@ -6,6 +8,74 @@ from msx_models.content import ContentItem, ContentRoot, Template
 from msx_models.utils import add_query_params
 
 router = APIRouter()
+
+
+class ContentBuilder:
+    content_json_url: str
+    player_json_url: str
+    jellyfin_client: JellyFinDep
+
+    def __init__(self, jellyfin_client, request: Request):
+        self.jellyfin_client = jellyfin_client
+        self.content_json_url = build_msx_uri(str(request.url_for("content_json")))
+        self.player_json_url = build_msx_uri(
+            str(request.url_for("player_json")),
+            ["ID", "PLATFORM", "PLAYER"]
+        )
+
+    def build(self, items: List[Dict]) -> ContentRoot:
+        all_episodes = all(item["Type"] == "Episode" for item in items)
+
+        if all_episodes:
+            return self._build_episodes_list(items)
+
+        content_items = []
+        for item in items:
+            match item["Type"]:
+                case "Series" | "Season" | "BoxSet":
+                    content_items.append(
+                        ContentItem(
+                            title=item["Name"],
+                            titleFooter=str(item.get("ProductionYear", "")),
+                            image=self.jellyfin_client.create_items_image_url(item["Id"], "Primary"),
+                            imageFiller="smart",
+                            action="content:" + add_query_params(self.content_json_url, dict(item_id=item["Id"]))
+                        )
+                    )
+                case "Movie" | "Video" | "MusicVideo":
+                    content_item = ContentItem(
+                        title=item["Name"],
+                        titleFooter=str(item.get("ProductionYear", "")),
+                        image=self.jellyfin_client.create_items_image_url(item["Id"], "Primary"),
+                        imageFiller="smart",
+                        action=f"execute:fetch:{add_query_params(self.player_json_url, dict(item_id=item['Id']))}"
+                    )
+
+                    content_items.append(content_item)
+
+        return ContentRoot(
+            type=ContentRoot.Type.pages.value,
+            template=Template(layout="0,0,3,6"),
+            items=content_items
+        )
+
+    def _build_episodes_list(self, items: List[Dict]) -> ContentRoot:
+        content_items = []
+        for item in items:
+            index = f"{item['IndexNumber']}. " if item.get("IndexNumber") else ""
+            content_item = ContentItem(
+                title=f"{index}{item["Name"]}",
+                image=self.jellyfin_client.create_items_image_url(item["Id"], "Primary"),
+                imageFiller="smart",
+                action=f"execute:fetch:{add_query_params(self.player_json_url, dict(item_id=item['Id']))}"
+            )
+            content_items.append(content_item)
+
+        return ContentRoot(
+            type=ContentRoot.Type.list.value,
+            template=Template(layout="0,0,12,2"),
+            items=content_items
+        )
 
 
 @router.get("/content.json")
@@ -19,36 +89,7 @@ async def content_json(request: Request, item_id: str, jellyfin_client: JellyFin
         )
     )
 
-    content_json_url = build_msx_uri(str(request.url_for("content_json")))
-    player_json_url = build_msx_uri(
-            str(request.url_for("player_json")),
-            ["ID", "PLATFORM", "PLAYER"]
-    )
-    content_items = []
+    content_builder = ContentBuilder(jellyfin_client, request)
+    content_root = content_builder.build(resp_items["Items"])
 
-    for item in resp_items["Items"]:
-        match item["Type"]:
-            case "Folder" | "Series" | "Season" | "BoxSet":
-                content_items.append(
-                    ContentItem(
-                        title=item["Name"],
-                        image=jellyfin_client.create_items_image_url(item["Id"], "Primary"),
-                        imageFiller="smart",
-                        action="content:" + add_query_params(content_json_url, dict(item_id=item["Id"]))
-                    )
-                )
-            case "Movie" | "Episode" | "Video" | "MusicVideo":
-                content_items.append(
-                    ContentItem(
-                        title=item["Name"],
-                        image=jellyfin_client.create_items_image_url(item["Id"], "Primary"),
-                        imageFiller="smart",
-                        action=f"execute:fetch:{add_query_params(player_json_url, dict(item_id=item['Id']))}"
-                    )
-                )
-
-    return ContentRoot(
-        type=ContentRoot.Type.pages.value,
-        template=Template(layout="0,0,3,6"),
-        items=content_items
-    )
+    return content_root
